b4c077d0ce09c1dc693a8b5f871f749d6d91d79f
Alexey Kholyavski <kholyavski@mail.ru>
2025-02-17 16:19:01 +0300

    update logic exam results

--- app/Services/ModuleExamUserResponseService.php
+++ app/Services/ModuleExamUserResponseService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\DataTransferObjects\ModuleExamUserResponseDTO;
+use App\Http\Requests\ExamUserResponseResultRequest;
 use App\Repositories\ModuleExamUserResponseRepository;
 
 class ModuleExamUserResponseService extends CoreService
@@ -30,4 +32,30 @@ class ModuleExamUserResponseService extends CoreService
             ]);
         }
     }
+
+    public function handleUserResponses(ExamUserResponseResultRequest $request, int $userId): void
+    {
+        $moduleExamId = $request->input('module_exam_id');
+        $this->repository->deleteUserResponses($moduleExamId, $userId);
+
+        $dtos = [];
+        foreach ($request->input('question_id') as $questionId) {
+            $answer = $request->input('answer')[$questionId] ?? null;
+
+            if (is_array($answer)) {
+                foreach ($answer as $moduleExamAnswerId) {
+                    $dtos[] = new ModuleExamUserResponseDTO($questionId, $userId, $moduleExamAnswerId, '', $moduleExamId);
+                }
+            } elseif (is_numeric($answer)) {
+                $dtos[] = new ModuleExamUserResponseDTO($questionId, $userId, (int) $answer, '', $moduleExamId);
+            } elseif (is_string($answer)) {
+                $dtos[] = new ModuleExamUserResponseDTO($questionId, $userId, null, $answer, $moduleExamId);
+            }
+        }
+
+        foreach ($dtos as $dto) {
+            $this->repository->createResponse($dto->toArray());
+        }
+    }
+
 }
