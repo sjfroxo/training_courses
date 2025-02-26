7d3e7ea604891ed7cc7c5a52f340a1164b62ec2b
Alexey Kholyavski <kholyavski@mail.ru>
2025-02-26 11:48:32 +0300

    refactoring

--- resources/views/user-study.blade.php
+++ resources/views/user-study.blade.php
@@ -28,16 +28,15 @@
                          role="tabpanel"
                          aria-labelledby="home-tab">
                         <div class="row g-3">
-                            <!-- Блок "Главная" -->
                             <div class="col-md-6">
-                                <div class="card p-3" style="border: none; background-color: #fff;">
+                                <div class="card p-3" style="border: none;">
                                     <h5 class="card-title">Главная</h5>
                                     <p class="card-text">Здесь отображается информация о вашем обучении, текущие курсы и прогресс.</p>
                                 </div>
                             </div>
 
                             <div class="col-md-6">
-                                <div class="card p-3" style="border: none; background-color: #fff;">
+                                <div class="card p-3" style="border: none;">
                                     <h5 class="card-title">Куратор</h5>
                                     <p class="card-text">Олег Петрович</p>
                                     <button class="btn btn-elprimary" style="width: 40%;">Написать сообщение</button>
@@ -47,8 +46,7 @@
 
                         <div class="mt-4">
                             <h5 class="mb-3">Активность</h5>
-                            <div class="card p-3" style="border: none; background-color: #fff;">
-{{--                                {{ $calendarHtml }}--}}
+                            <div class="card p-3" style="border: none;">
                             </div>
                         </div>
                     </div>
@@ -56,7 +54,7 @@
                          id="progress"
                          role="tabpanel"
                          aria-labelledby="progress-tab">
-                        <div class="card p-3" style="border: none; background-color: #fff;">
+                        <div class="card p-3" style="border: none;">
                             <h5 class="card-title">Успеваемость</h5>
                             <p class="card-text">Здесь вы можете увидеть свои результаты и статистику по курсам.</p>
                         </div>
@@ -65,7 +63,7 @@
                          id="tasks"
                          role="tabpanel"
                          aria-labelledby="tasks-tab">
-                        <div class="card p-3" style="border: none; background-color: #fff;">
+                        <div class="card p-3" style="border: none;">
                             <h5 class="card-title">Задания</h5>
                             <p class="card-text">Список заданий, которые нужно выполнить в рамках курсов.</p>
                         </div>
