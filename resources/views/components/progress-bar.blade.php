36c2b8011f2f18a2204cbbcd6729176db7b1b98a
Alexey Kholyavski <kholyavski@mail.ru>
2025-03-07 15:45:53 +0300

    update progress bar

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,13 +1,17 @@
 <div>
     <h5>Прогресс:</h5>
-    <div
-        class="progress"
-        role="progressbar"
-        aria-label="Example with label"
-        aria-valuenow="{{$progress}}"
-        aria-valuemin="0"
-        aria-valuemax="100"
-    >
-        <div class="progress-bar" style="width: {{$progress}}%">{{$progress}}%</div>
-    </div>
+    @if(is_numeric($progress) && $progress >= 0 && $progress <= 100)
+        <div
+            class="progress"
+            role="progressbar"
+            aria-label="Example with label"
+            aria-valuenow="{{ $progress }}"
+            aria-valuemin="0"
+            aria-valuemax="100"
+        >
+            <div class="progress-bar" style="width: {{ $progress }}%">{{ $progress }}%</div>
+        </div>
+    @else
+        <p>Прогресс не доступен</p>
+    @endif
 </div>
