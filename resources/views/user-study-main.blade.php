3a2c7ff2e9263baec0195cfd146e417d39e3c8a6
Alexey Kholyavski <kholyavski@mail.ru>
2025-04-15 12:27:54 +0300

    calendar

--- resources/views/user-study-main.blade.php
+++ resources/views/user-study-main.blade.php
@@ -49,7 +49,31 @@
 
                     <div class="mt-4">
                         <h4 class="mb-3">Активность</h4>
-                        <div class="card p-3" style="border: none;"></div>
+                        <div class="calendar">
+                            <div class="calendar-header">
+                                <a href="{{ route(request()->route()->getName(), [Auth::id(), $course->id, $calendarData['prevMonth'], $calendarData['prevYear']]) }}" class="btn btn-sm btn-outline-secondary">&lt;</a>
+                                <span>{{ $calendarData['firstDay']->format('F Y') }}</span>
+                                <a href="{{ route(request()->route()->getName(), [Auth::id(), $course->id, $calendarData['nextMonth'], $calendarData['nextYear']]) }}" class="btn btn-sm btn-outline-secondary">&gt;</a>
+                            </div>
+                            <div class="calendar-body d-flex flex-wrap">
+                                @foreach (['Пн','Вт','Ср','Чт','Пт','Сб','Вс'] as $day)
+                                    <div class="text-center fw-bold" style="width: calc(100% / 7);">{{ $day }}</div>
+                                @endforeach
+
+                                @for ($i = 1; $i < $calendarData['startingDay']; $i++)
+                                    <div style="width: calc(100% / 7);" class="p-2 text-center">&nbsp;</div>
+                                @endfor
+
+                                @for ($day = 1; $day <= $calendarData['daysInMonth']; $day++)
+                                    @php
+                                        $isActive = in_array($day, $calendarData['activeDays']);
+                                    @endphp
+                                    <div style="width: calc(100% / 7);" class="p-2 text-center rounded {{ $isActive ? 'bg-primary text-white' : '' }}">
+                                        {{ $day }}
+                                    </div>
+                                @endfor
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
